87346e93d749282973a45224283bc92acf165b31
Siddhant Prakash <sid.rol92@gmail.com>
2017-02-17 18:43:01 -0700

    mp3 submission update

--- miniproject3/neural.py
+++ miniproject3/neural.py
@@ -1,50 +1,57 @@
 from yann.network import network
+import time
 
-# create a network
-net = network()
-dataset_params = {"dataset": "_datasets/_dataset_46006", "id": 'mnist', "n_classes": 10}
-net.add_layer(type = "input", id = "input", dataset_init_args = dataset_params)
 
-momentum = ["polyak","nesterov","false"]
+momentum = ["false","polyak","nesterov"]
 optim = ["sgd","adagrad","rmsprop"]
 lr = [(0.05,0.01,0.001),(0.1,0.05,0.01),(0.01,0.001,0.0001)]
 
-# adding layers
-
-net.add_layer(type = "dot_product", 
-	origin = "input", 
-	id = "dot_product_1", 
-	num_neurons = 800, 
-	regularize = True, 
-	activation = 'relu'
-	)
-
-net.add_layer(type = "dot_product",
-	origin = "dot_product_1",
-	id = "dot_product_2",
-	num_neurons = 800,
-	regularize = True,
-	activation = 'relu'
-	)
-
-net.add_layer(type = "classifier",
-	id = "softmax",
-	origin = "dot_product_2",
-	num_classes = 10,
-	activation = "softmax"
-	)
-
-net.add_layer(type = "objective",
-	id = "nil",
-	origin = "softmax")
-
-net.pretty_print()
+count = 1
 
 for k in range(len(lr)):
 	for i in range(len(momentum)):
 		for j in range(len(optim)):
 
+			# create a network
+			net = network()
+			dataset_params = {"dataset": "_datasets/_dataset_46006", "id": 'mnist', "n_classes": 10}
+			net.add_layer(type = "input", id = "input", dataset_init_args = dataset_params)
+
+			# adding layers
+
+			net.add_layer(type = "dot_product", 
+				origin = "input", 
+				id = "dot_product_1", 
+				num_neurons = 800, 
+				regularize = True, 
+				activation = 'relu'
+				)
+
+			net.add_layer(type = "dot_product",
+				origin = "dot_product_1",
+				id = "dot_product_2",
+				num_neurons = 800,
+				regularize = True,
+				activation = 'relu'
+				)
+
+			net.add_layer(type = "classifier",
+				id = "softmax",
+				origin = "dot_product_2",
+				num_classes = 10,
+				activation = "softmax"
+				)
+
+			net.add_layer(type = "objective",
+				id = "nil",
+				origin = "softmax")
+
+			net.pretty_print()
+
+
 			id = momentum[i]+'-'+optim[j]
+			print "-----",count,id,"-------"
+
 			optimizer_params = { 
 				"momentum_type" 	: momentum[i],
 				"momentum_params" 	: (0.6,0.95, 30),
@@ -72,6 +79,7 @@ for k in range(len(lr)):
 
 			net.test()
 
-			print "------------------",id,lr[1],"------------------"
+			print "------------------",id,lr[k],"------------------"
 			print "================================================"
+			time.sleep(150)
 
